c434a989cc5d2639ee6e6672b4ab403f3e792b6b
Sangeetha Nandakumar <98574336+sangeethankumar@users.noreply.github.com>
2024-08-27 14:52:55 +0200

    analysis

--- dataset_explore/download_data.py
+++ dataset_explore/download_data.py
@@ -1,5 +1,5 @@
 from dandi.download import download
 
 def download_data(dandi_id):
-    url = "https://api.dandiarchive.org/api/assets/" + dandi_id + "download/"
+    url = "https://api.dandiarchive.org/api/assets/" + dandi_id + "/download/"
     download(url, '.')
